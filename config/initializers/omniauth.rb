31617f654f97df82d4809792e50baa4124176594
Claudio Kerekes <claudio.kerekes@gmail.com>
2016-06-10 14:52:41 -0500

    configuración omniauth,para google

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, Rails.application.secrets.omniauth_twitter_provider_key,Rails.application.secrets.omniauth_twitter_provider_secret
   provider :google_oauth2, Rails.application.secrets.omniauth_google_provider_key,Rails.application.secrets.omniauth_google_provider_secret
 
-  OmniAuth.config.full_host = Rails.env.production? ? 'http://miedificio.herokuapp.com' : 'http://localhost:3000'
+  OmniAuth.config.full_host = Rails.env.production? ? 'http://omniauthfacetwit.herokuapp.com' : 'http://localhost:3000'
 
 end
